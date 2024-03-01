9e6402e1d746a1ac221dff59353b295f5804ae6d
MatheusMorata <matlucar09@hotmail.com>
2024-03-01 20:01:29 -0300

    feat questao1.js

--- questao1.js
+++ questao1.js
@@ -34,7 +34,23 @@ class Vertice{
 
     //Método equals
     equals(outroVertice) {
-        return this.#x === outroVertice.x && this.#y === outroVertice.y;
+        if (this.#x === outroVertice.x && this.#y === outroVertice.y){
+            return "Sao iguais"; 
+        }else{
+            return "Sao diferentes";
+        }
     }
-
 }
+
+//Leitura de 3 vértices
+const v1 = new Vertice(1,1);
+const v2 = new Vertice(4,5);
+const v3 = new Vertice(3,2);
+
+console.log("Distancia de v1 e v2: ", v1.distancia(v2));
+
+console.log("v2 e v3 sao: ",v2.equals(v3));
+
+v3.move(4,5);
+
+console.log("v2 e v3 sao: ",v2.equals(v3));
