96b644520a0945853b4d29705ce6bc91dfd38bd4
MatheusMorata <matlucar09@hotmail.com>
2024-03-01 19:40:55 -0300

    feat questao1.js

---
+++ questao1.js
@@ -0,0 +1,40 @@
+class Vertice{
+    //Atributos privados -> #
+    #x;
+    #y;
+
+    //Método construtor
+    constructor(x, y){
+        this.#x = x;
+        this.#y = y;
+    }
+
+    //Getters
+    get x(){
+        return this.#x;
+    }
+
+    get y(){
+        return this.#y;
+    }
+
+    get distancia() {
+        return function(outroVertice) {
+            const deltaX = outroVertice.x - this.#x;
+            const deltaY = outroVertice.y - this.#y;
+            return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+    }
+
+    //Método move
+    move(x,y){
+        this.#x = x;
+        this.#y = y;
+    }
+
+    //Método equals
+    equals(outroVertice) {
+        return this.#x === outroVertice.x && this.#y === outroVertice.y;
+    }
+
+}
