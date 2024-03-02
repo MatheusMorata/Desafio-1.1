5e15b7d35b7a1e5e9b6a3fc1455cf039d483824e
MatheusMorata <matlucar09@hotmail.com>
2024-03-02 14:21:50 -0300

    feat questao1.js

--- questao1.js
+++ questao1.js
@@ -41,3 +41,16 @@ class Vertice{
         }
     }
 }
+
+//Leitura de 3 vértices
+const v1 = new Vertice(1,1);
+const v2 = new Vertice(4,5);
+const v3 = new Vertice(3,2);
+
+console.log("Distancia de v1 e v2: ", v1.distancia(v2));
+
+console.log("v2 e v3 sao: ",v2.equals(v3));
+
+v3.move(4,5);
+
+console.log("v2 e v3 sao: ",v2.equals(v3));
