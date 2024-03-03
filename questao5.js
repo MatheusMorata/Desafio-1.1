0c659da6f2ba8b48b9683adc94c67a82df17966a
MatheusMorata <matlucar09@hotmail.com>
2024-03-03 15:20:00 -0300

    feat questao5.js

--- questao5.js
+++ questao5.js
@@ -1,1 +1,8 @@
 const read = require('readline-sync');
+
+let nome = read.question("Digite o nome: ");
+let CPF = read.question("Digite o CPF: ");
+let nascimento = read.question("Digite a data de nascimento: ");
+let renda = read.question("Digite a renda mensal: ");
+let estado_civ = read.question("Digite o estado civil: ");
+let dependentes = read.question("Digite o numero de dependentes: ");
