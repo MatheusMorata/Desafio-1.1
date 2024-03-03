5b1286bcda1c303bb5acb6c626d0c1262c14dc55
MatheusMorata <matlucar09@hotmail.com>
2024-03-03 17:17:37 -0300

    feat questao5.js

--- questao5.js
+++ questao5.js
@@ -5,37 +5,24 @@ class Pessoa{
     #CPF;
     #nascimento;
     #renda;
-    #estado_civ;
+    #estado_civil;
     #dependentes;
 
-    constructor(n,cpf,nasc,renda,est,depen){
-        this.validarDependentes(depen);
-        this.validarCPF(cpf);
+    constructor(n,cpf,dia,mes,ano,renda,est,depen){
         this.validarEstadoCivil(est);
         this.validarRenda(renda);
         this.validarNome(n);
-        this.validarDataNascimento(nasc);
 
         this.#nome = n;
         this.#CPF = cpf;
-        this.#nascimento = nasc;
+        this.#nascimento = dia+'/'+mes+'/'+ano;
         this.#renda = renda;
-        this.#estado_civ = est.toUpperCase();
+        this.#estado_civil = est.toUpperCase();
         this.#dependentes = depen;
     }
 
 
-    validarDependentes(dependentes) {
-        if (dependentes < 0 || dependentes > 10) {
-            throw new Error("O número de dependentes deve estar entre 0 e 10.");
-        }
-    }
-
-    validarCPF(cpf) {
-        if (cpf.length !== 11 || !/^\d+$/.test(cpf)) {
-            throw new Error("CPF inválido. Deve conter apenas números e ter tamanho igual a 11.");
-        }
-    }
+    
 
     validarEstadoCivil(estado_civil) {
         if (!estado_civil || !/^[C|S|V|D]$/i.test(estado_civil)) {
@@ -55,36 +42,31 @@ class Pessoa{
         }
     }
 
-    validarDataNascimento(nascimento) {
-        if (!nascimento || !/\d{2}\/\d{2}\/\d{4}/.test(nascimento)) {
-            throw new Error("Data de nascimento inválida. Deve estar no formato DD/MM/AAAA.");
-        }
-
-        const partesData = nascimento.split('/');
-        const dia = parseInt(partesData[0], 10);
-        const mes = parseInt(partesData[1], 10);
-        const ano = parseInt(partesData[2], 10);
-
-        const dataNascimento = new Date(ano, mes - 1, dia);
-        const dataAtual = new Date();
-        const idade = dataAtual.getFullYear() - dataNascimento.getFullYear();
-
-        if (dataAtual.getMonth() < dataNascimento.getMonth() || 
-            (dataAtual.getMonth() === dataNascimento.getMonth() && dataAtual.getDate() < dataNascimento.getDate())) {
-            idade--;
-        }
-
-        if (idade < 18) {
-            throw new Error("O cliente deve ter pelo menos 18 anos na data atual.");
-        }
-    }
+    toString(){
+        console.clear();
+        const saida = 'Nome: '+this.#nome+
+                      '\nCPF: '+this.#CPF+
+                      '\nData de nascimento: '+this.#nascimento+
+                      '\nRenda Mensal: '+this.#renda+
+                      '\nEstado cívil: '+this.#estado_civil+
+                      '\nDependentes: '+this.#dependentes;
+        return saida;
+    } 
 
 }
 
 let nome = read.question("Digite o nome: ");
 let CPF = read.question("Digite o CPF: ");
-let nascimento = read.question("Digite a data de nascimento: ");
+let dia = read.question("Digite o dia de nascimento: ");
+let mes = read.question("Digite o mes de nascimento: ");
+let ano = read.question("Digite o ano de nascimento: ")
 let renda = read.question("Digite a renda mensal: ");
 let estado_civ = read.question("Digite o estado civil: ");
 let dependentes = read.question("Digite o numero de dependentes: ");
 
+try{
+    const p = new Pessoa(nome,CPF,dia,mes,ano,renda,estado_civ,dependentes);
+    console.log(p.toString());
+}catch(error){
+    console.error(error.message);
+}
