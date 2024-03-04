6029964341aab83787419b5f4173d3bc7aae6b47
MatheusMorata <matlucar09@hotmail.com>
2024-03-04 20:01:55 -0300

    feat questao4.js

--- questao4.js
+++ questao4.js
@@ -4,10 +4,26 @@ class Aluno{
     #p1;
     #p2;
 
-    
+    constructor(m,n,p1,p2){
+        if (m.trim() === '' || n.trim() === '') {
+            throw new Error('A matrícula e o nome não podem estar vazios.');
+        }
+        this.#matricula = m;
+        this.#nome = n;
+        this.#p1 = p1;
+        this.#p2 = p2;
+    }
+
+    // SETTER
+    set p1(p1){
+        this.#p1 = p1;
+    }
+
+    set p2(p2){
+        this.#p2 = p2;
+    }
 
 }
-
 
 class Turma{
     #alunos;
