616ec797d49d5cc89892a313a35c1a0f5d75f84f
MatheusMorata <matlucar09@hotmail.com>
2024-03-11 17:58:12 -0300

    feat main.js

--- Desafio#1.2/main.js
+++ Desafio#1.2/main.js
@@ -1,5 +1,26 @@
+const read = require('readline-sync');
 const Menus = require('./menus');
 
 const menus = new Menus();
+let op = 0;
+let op_paciente = 0;
+let op_agenda = 0;
 
-menus.menuPrincipal();
+while(op != 3){
+    menus.menuPrincipal();
+    op = read.question("Opcao: ");
+    console.clear();
+    if(op === 1){
+        while(op_paciente != 5){
+
+        }
+    }else if(op === 2){
+        while(op_agenda != 4){
+
+        }
+    }else if(op === 3){
+        console.log("Até logo...");
+    }else{
+        console.log("Opcao inválida...");
+    }
+}
