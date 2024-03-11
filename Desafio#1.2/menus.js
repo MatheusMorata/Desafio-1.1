372a6b177898542dd8a605e6f2242849446a4355
MatheusMorata <matlucar09@hotmail.com>
2024-03-11 17:40:47 -0300

    feat menus.js

--- Desafio#1.2/menus.js
+++ Desafio#1.2/menus.js
@@ -1,6 +1,9 @@
-function menuPrincipal(){
-    console.log("Menu Principal ");
-    console.log("1-Cadastro de pacientes");
-    console.log("2-Agenda");
-    console.log("3-Fim");
+class Menus{
+    menuPrincipal(){
+        console.log("Menu Principal ");
+        console.log("1-Cadastro de pacientes");
+        console.log("2-Agenda");
+        console.log("3-Fim");
+    }
 }
+module.export = Menus;
