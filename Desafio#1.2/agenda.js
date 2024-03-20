850153109642950356138af955d894e812389640
MatheusMorata <matlucar09@hotmail.com>
2024-03-20 19:27:48 -0300

    feat agenda.js

--- Desafio#1.2/agenda.js
+++ Desafio#1.2/agenda.js
@@ -1,3 +1,26 @@
-class Agenda{
+const Paciente = require('./paciente');
 
+class Agendamento {
+    #consultas
+
+    constructor() {
+        this.#consultas = [];
+    }
+
+    agendarConsulta(cpf, hora, minuto) {
+        const paciente = Paciente.getPorCPF(cpf); 
+
+        if (!paciente) {
+            throw new Error("CPF não encontrado no cadastro.");
+        }
+
+
+        this.#consultas.push({
+            cpf: cpf,
+            hora: hora,
+            minuto: minuto
+        });
+
+        console.log("Consulta agendada com sucesso!");
+    }
 }
