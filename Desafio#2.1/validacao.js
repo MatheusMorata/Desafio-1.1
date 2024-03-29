fb175349f46b3c2b569aaadf2d59cf38772ed104
MatheusMorata <matlucar09@hotmail.com>
2024-03-29 13:16:12 -0300

    delete cliente.js

--- Desafio#2.1/validacao.js
+++ Desafio#2.1/validacao.js
@@ -1,2 +1,19 @@
-const args = process.argv.slice(2);
+const fs = require('fs');
+try{
+    const caminho = process.argv[2]; // Acessando o primeiro elemento do array
+    fs.readFile(caminho, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Erro ao ler o arquivo:', err);
+            return;
+        }
+    
+        // Converte o conteúdo do arquivo para um objeto JavaScript
+        const objetoJSON = JSON.parse(data);
+    
+        // Faça o que você quiser com o objeto JSON
+        console.log('Conteúdo do arquivo JSON:', objetoJSON);
+    });
+}catch(error){
+    console.log("ERRO: Não passou o caminho do arquivo.");
+}
 
