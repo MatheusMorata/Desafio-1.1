5cc7e279aa6178ce0d75e6c99f52ad960df06811
MatheusMorata <matlucar09@hotmail.com>
2024-03-29 14:01:55 -0300

    feat validacao.js

--- Desafio#2.1/validacao.js
+++ Desafio#2.1/validacao.js
@@ -6,13 +6,17 @@ try{
             console.error('Erro ao ler o arquivo:', err);
             return;
         }
-    
         // Converte o conteúdo do arquivo para um objeto JavaScript
         const objetoJSON = JSON.parse(data);
-    
-        // Faça o que você quiser com o objeto JSON
-        console.log('Conteúdo do arquivo JSON:', objetoJSON);
     });
 }catch(error){
     console.log("ERRO: Não passou o caminho do arquivo.");
+}
+
+
+for(let i = 0; objetoJSON.length > i; i++){
+    console.log("Nome: "+objetoJSON[i].nome);
+    console.log("CPF: "+objetoJSON[i].cpf);
+    console.log("Data de nascimento: "+objetoJSON[i].dt_nascimento);
+    console.log("");
 }
