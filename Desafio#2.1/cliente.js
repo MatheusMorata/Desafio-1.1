a7371831dbc61d27a072084d011e4b5af78d06d9
MatheusMorata <matlucar09@hotmail.com>
2024-03-29 11:01:10 -0300

    Inciando desafio2.1

---
+++ Desafio#2.1/cliente.js
@@ -0,0 +1,17 @@
+class Cliente{
+    #nome;
+    #cpf;
+    #nascimento;
+    #renda;
+    #est_civil;
+
+    Cliente(n,cpf,nasc,r,est){
+        this.#nome = n;
+        this.#cpf = cpf;
+        this.#nascimento = nasc;
+        this.#renda = r;
+        this.#est_civil = est;
+    }
+
+    
+}
