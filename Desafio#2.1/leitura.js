7065f8cb87feb7e503eda0ab013cda5031667c06
MatheusMorata <matlucar09@hotmail.com>
2024-03-29 15:09:43 -0300

    feat validacao.js

--- Desafio#2.1/leitura.js
+++ Desafio#2.1/leitura.js
@@ -1,21 +1,16 @@
 const fs = require('fs');
 
-class Leitura{
-    ler_arquivo(caminho){
-        try{
-            fs.readFile(caminho, 'utf8', (err, data) => {
-                if (err) {
-                    console.error('Erro ao ler o arquivo:', err);
-                    return;
-                }
-                // Converte o conteúdo do arquivo para um objeto JavaScript
-                const objetoJSON = JSON.parse(data);
-                return objetoJSON;
-            });
-        }catch(error){
-            console.log("ERRO: Não passou o caminho do arquivo ou arquivo inexistente.");
+class LeitorJSON {
+    lerJSON(caminhoArquivo) {
+        try {
+            const conteudoArquivo = fs.readFileSync(caminhoArquivo, 'utf8');
+            const objetoJavaScript = JSON.parse(conteudoArquivo);
+            return objetoJavaScript;
+        } catch (error) {
+            console.error('Erro ao ler o arquivo:', error);
+            return null;
         }
     }
 }
 
-module.exports = Leitura;
+module.exports = LeitorJSON;
--- Desafio#2.1/validacao.js
+++ Desafio#2.1/validacao.js
@@ -1,1 +1,6 @@
+const Leitura = require('./leitura');
 const caminho = process.argv[2]; 
+const leitor = new Leitura();
+
+const objeto = leitor.lerJSON(caminho);
+console.log(objeto);
