7396f468b9180d36161a9b0f3235663d59773fb2
MatheusMorata <matlucar09@hotmail.com>
2024-03-29 14:59:48 -0300

    feat leitura.js

--- Desafio#2.1/leitura.js
+++ Desafio#2.1/leitura.js
@@ -1,9 +1,8 @@
 const fs = require('fs');
 
 class Leitura{
-    ler_arquivo(){
+    ler_arquivo(caminho){
         try{
-            const caminho = process.argv[2]; // Acessando o primeiro elemento do array
             fs.readFile(caminho, 'utf8', (err, data) => {
                 if (err) {
                     console.error('Erro ao ler o arquivo:', err);
@@ -11,11 +10,12 @@ class Leitura{
                 }
                 // Converte o conteúdo do arquivo para um objeto JavaScript
                 const objetoJSON = JSON.parse(data);
-                
+                return objetoJSON;
             });
-            return objetoJSON;
         }catch(error){
             console.log("ERRO: Não passou o caminho do arquivo ou arquivo inexistente.");
         }
     }
 }
+
+module.exports = Leitura;
