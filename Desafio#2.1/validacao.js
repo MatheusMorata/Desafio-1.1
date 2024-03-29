dddb823cd58cb22b22010ec1e9bbe0e18b4fc530
MatheusMorata <matlucar09@hotmail.com>
2024-03-29 14:37:46 -0300

    feat validacao.js

--- Desafio#2.1/validacao.js
+++ Desafio#2.1/validacao.js
@@ -8,15 +8,14 @@ try{
         }
         // Converte o conteúdo do arquivo para um objeto JavaScript
         const objetoJSON = JSON.parse(data);
+        for(let i = 0; objetoJSON.length > i; i++){
+            console.log("Nome: "+objetoJSON[i].nome);
+            console.log("CPF: "+objetoJSON[i].cpf);
+            console.log("Data de nascimento: "+objetoJSON[i].dt_nascimento);
+            console.log("Renda mensal: "+objetoJSON[i].renda_mensal);
+            console.log("Estado cívil: "+objetoJSON[i].estado_civil);
+        }
     });
 }catch(error){
-    console.log("ERRO: Não passou o caminho do arquivo.");
-}
-
-
-for(let i = 0; objetoJSON.length > i; i++){
-    console.log("Nome: "+objetoJSON[i].nome);
-    console.log("CPF: "+objetoJSON[i].cpf);
-    console.log("Data de nascimento: "+objetoJSON[i].dt_nascimento);
-    console.log("");
+    console.log("ERRO: Não passou o caminho do arquivo ou arquivo inexistente.");
 }
