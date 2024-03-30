35e864daaa13c44e8c4b9021f87b0fae19ce4be7
MatheusMorata <matlucar09@hotmail.com>
2024-03-30 14:27:31 -0300

    feat validacao.js

--- Desafio#2.1/validacao.js
+++ Desafio#2.1/validacao.js
@@ -1,1 +1,7 @@
-let dados = []
+const Arquivo = require('./arquivo');
+const caminho = process.argv[2];
+
+const arq = new Arquivo();
+const teste = arq.constructor.lerJSON(caminho);
+
+console.log(teste);
