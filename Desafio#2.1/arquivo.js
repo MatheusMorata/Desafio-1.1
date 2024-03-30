13cf21dc47bc22616ac21e7f2aff4b32c7ae984b
MatheusMorata <matlucar09@hotmail.com>
2024-03-30 14:37:28 -0300

    feat arquivo\validacao

--- Desafio#2.1/arquivo.js
+++ Desafio#2.1/arquivo.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
+
 class Arquivo {
     static lerJSON(caminhoArquivo) {
         try {
+            // Verifica se o caminho é de fato um arquivo
+            const stat = fs.statSync(caminhoArquivo);
+            if (!stat.isFile()) {
+                throw new Error('O caminho especificado não é um arquivo.');
+            }
+
+            // Lê o arquivo JSON
             const dados = fs.readFileSync(caminhoArquivo, 'utf8');
             const objetoJavaScript = JSON.parse(dados);
             return objetoJavaScript;
         } catch (erro) {
-            console.error('Ocorreu um erro ao ler o arquivo JSON:', erro);
-            return null;
+            // Altera a mensagem de erro
+            throw new Error('O caminho especificado não é um arquivo.');
         }
     }
 }
--- Desafio#2.1/validacao.js
+++ Desafio#2.1/validacao.js
@@ -1,6 +1,11 @@
 const Arquivo = require('./arquivo');
-const caminho = process.argv[2];
-
 const arq = new Arquivo();
-const teste = arq.constructor.lerJSON(caminho);
-
+try {
+    const caminho = process.argv[2];
+    if (!caminho) {
+        throw new Error('Caminho do arquivo não fornecido.');
+    }
+    const teste = arq.constructor.lerJSON(caminho);
+} catch (error) {
+    console.log("ERRO:", error.message);
+}
