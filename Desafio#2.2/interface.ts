042888be492659f32aba87172103b1ab64a3df36
MatheusMorata <matlucar09@hotmail.com>
2024-04-06 18:57:26 -0300

    add dados.ts / refactoring interface.ts

--- Desafio#2.2/interface.ts
+++ Desafio#2.2/interface.ts
@@ -1,11 +1,9 @@
 // Importações
 import * as read from 'readline-sync';
 import { Validacao } from './validacao';
-import { Api } from './api';
- 
+
 // Objetos 
 const validar = new Validacao();
-const api = new Api();
 
 // Classe Interface
 export class Interface {
@@ -33,7 +31,6 @@ export class Interface {
                     valor = parseFloat(read.question("Valor: "));
                     validar.validarValor(valor);
 
-                    api.getCotacao(moedaOrigem,moedaDestino);
 
                 } else {
                     break;
