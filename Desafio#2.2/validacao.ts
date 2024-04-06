0360f384dd661dffc1e20c767895e6ab27d84cf8
MatheusMorata <matlucar09@hotmail.com>
2024-04-06 10:24:51 -0300

    feat validacao.ts

--- Desafio#2.2/validacao.ts
+++ Desafio#2.2/validacao.ts
@@ -1,14 +1,28 @@
 export class Validacao{
     
+    // Moeda de origem e de destino devem ter exatamente 3 caracteres.
     validarLengthCaracteres(str){
         if(str.length != 3 && str.length != 0){
             throw new Error("ERRO: A string deve ter exatos 3 caracteres. ");
         }
     }
 
+    // Moeda origem ≠ moeda destino.
     validarMoedas(str1,str2){
         if(str1 == str2){
             throw new Error("ERRO: Não é possível fazer conversão da mesma moeda");
         }
     }
+
+    // O valor deve ser númerico.
+    validarValor(valor: any): number {
+        // Verifica se o valor não é um número ou se não é um valor de ponto flutuante
+        if (isNaN(valor) || typeof valor !== 'number' || !Number.isFinite(valor)) {
+            throw new Error('O valor não é um número de ponto flutuante.');
+        }
+    
+        return parseFloat(valor.toFixed(2)); // Retorna o valor com duas casas decimais
+    }
+    
+
 }
