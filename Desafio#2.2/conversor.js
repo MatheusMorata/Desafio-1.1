eb6927d1177bcfa6236f9a12d7ae72ad083fdb04
MatheusMorata <matlucar09@hotmail.com>
2024-04-07 10:45:27 -0300

    deleta .js

--- Desafio#2.2/conversor.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Importações
-var interface_1 = require("./interface");
-var objeto_interface = new interface_1.Interface();
-objeto_interface.menu();
