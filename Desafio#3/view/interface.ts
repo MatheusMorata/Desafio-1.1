5017febf07667afaed7966658f441c4af82bbc17
MatheusMorata <matlucar09@hotmail.com>
2024-04-20 16:03:17 -0300

    feat interface.ts

--- Desafio#3/view/interface.ts
+++ Desafio#3/view/interface.ts
@@ -1,1 +1,29 @@
 import read from 'readline-sync';
+
+export class Interface{
+    
+    MenuPrincipal(){
+        console.log("Menu Principal");
+        console.log("1-Cadastro de pacientes");
+        console.log("2-Agenda");
+        console.log("3-Fim");
+    }
+
+    MenuCadastroPaciente(){
+        console.log("Menu do cadastro de Pacientes");
+        console.log("1-Cadastrar novo paciente");
+        console.log("2-Excluir paciente");
+        console.log("3-Listar pacientes (ordenado por CPF)");
+        console.log("4-Listar pacientes (ordenado por nome)");
+        console.log("5-Voltar p/ menu principal");
+    }
+
+    MenuCadastroAgenda(){
+        console.log("Agenda");
+        console.log("1-Agendar consulta");
+        console.log("2-Cancelar agendamento");
+        console.log("3-Listar agenda");
+        console.log("4-Voltar p/ menu principal");
+    }
+    
+}
