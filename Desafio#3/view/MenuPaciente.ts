1c6d92eb4663af01e2106a75958df2d0132c02d2
MatheusMorata <matlucar09@hotmail.com>
2024-04-22 12:51:17 -0300

    feat MenuPaciente.ts

--- Desafio#3/view/MenuPaciente.ts
+++ Desafio#3/view/MenuPaciente.ts
@@ -2,6 +2,20 @@ import * as read from 'readline-sync';
 
 export class MenuPaciente{
 
+    ExibirCadastrarPaciente(){
+        console.log("CADASTRO");
+        const cpf: string = read.question("Digite o CPF: (XXXXXXXXXXX)");
+        const nome: string = read.question("Digite o nome: ");
+        const dataNascimento: string = read.question("Digite a data de nascimento: (DD/MM/AAAA)");
+        console.clear();
+    }
+
+    ExibirExcluirPaciente(){
+        console.log("EXCLUIR");
+        const cpf: string = read.question("Digite o CPF: ");
+        console.clear();
+    }
+
     Menu():void{
 
         let ativo: number = 0;
@@ -16,9 +30,9 @@ export class MenuPaciente{
             console.clear();
             
             if(ativo == 1){
-                console.log("Cadastrar paciente");
+                this.ExibirCadastrarPaciente();
             }else if(ativo == 2){
-                console.log("Excluir paciente");
+                this.ExibirExcluirPaciente();
             }else if(ativo == 3){
                 console.log("Listar por");
             }else if(ativo == 4){
