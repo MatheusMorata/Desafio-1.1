9f8afd1593da175655fa990ae3c213906d09842e
MatheusMorata <matlucar09@hotmail.com>
2024-04-22 13:49:11 -0300

    feat paciente.ts

---
+++ Desafio#3/model/paciente.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Model } from 'sequelize';
+import { sql } from '../database/db'; // Verifique o caminho do seu arquivo de conexão
+
+class Paciente extends Model {
+  public cpf!: string;
+  public nome!: string;
+  public dataNascimento!: string;
+
+  // Adicione métodos de classe ou de instância, se necessário
+}
+
+Paciente.init({
+  cpf: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  dataNascimento: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  sequelize: sql,
+  modelName: 'Paciente'
+});
+
+export { Paciente };
