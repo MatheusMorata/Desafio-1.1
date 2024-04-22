932d1dff5a0232df4b85983022c56e57505efc11
MatheusMorata <matlucar09@hotmail.com>
2024-04-22 14:48:51 -0300

    refactoring

--- Desafio#3/database/db.ts
+++ Desafio#3/database/db.ts
@@ -1,9 +1,10 @@
 import { Sequelize } from 'sequelize';
 
-const sql = new Sequelize('', 'postgres', '123', {
+const sql = new Sequelize('Desafio3', 'postgres', '123', {
   host: 'localhost',
   dialect: 'postgres',
   port: 5432 
 });
+
 
 export {sql};
